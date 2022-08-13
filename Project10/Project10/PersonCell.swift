75575f7726659bda3ce9a2a125b21fb4c40f1d22
Rishi Chhabra <56560201+rishichhabra08@users.noreply.github.com>
2022-08-13 21:53:52 +0530

    Project10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Rishi Chhabra on 13/08/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBOutlet var name: UILabel!
+    
+    
+}
