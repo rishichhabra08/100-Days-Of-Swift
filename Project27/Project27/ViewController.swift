8fe7c8a9bc943cc344cb3b90eaf17ef53accd154
Rishi Chhabra <56560201+rishichhabra08@users.noreply.github.com>
2022-08-20 21:15:18 +0530

    Project27

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,176 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Rishi Chhabra on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawLines()
+    }
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if (currentDrawType > 5) {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+            
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerBoard()
+        case 3:
+            drawRotatedSquares()
+        case 4:
+            drawLines()
+        case 5:
+            drawImagesAndText()
+        default:
+            break
+        }
+    }
+    
+   
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0,width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0,width: 512, height: 512).insetBy(dx: 5, dy: 5)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCheckerBoard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0..<8 {
+                for col in 0..<8 {
+                    if (row+col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col*64, y: row*64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { context in
+            
+            context.cgContext.translateBy(x: 256, y: 256)
+            
+            let rotations = 16
+            let amount  = Double.pi / Double(rotations)
+        
+            
+            for _ in 0..<rotations {
+                context.cgContext.rotate(by: CGFloat(amount))
+                context.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.strokePath()
+            
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { context in
+            context.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0..<256{
+                context.cgContext.rotate(by: .pi/2)
+                
+                if first {
+                    context.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    context.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                
+                length *= 0.99
+            }
+            
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.strokePath()
+            
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawImagesAndText() {
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+            let img = renderer.image { ctx in
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = .center
+
+                let attrs = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSAttributedString.Key.paragraphStyle: paragraphStyle]
+
+                let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+                string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
+
+                let mouse = UIImage(named: "mouse")
+                mouse?.draw(at: CGPoint(x: 300, y: 150))
+            }
+
+            imageView.image = img
+        }
+
+    
+}
+
